a5a5025de9f7bfa5430d19f16173a25262d2c23b
Swacha Mallik <swacha.lab19@gmail.com>
2026-09-11 05:22:36 -0230

    Grant API permission to publish SQS test runs

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -170,6 +170,47 @@ resource "aws_iam_role" "ecs_execution" {
   })
 }
 
+resource "aws_iam_role" "api_task" {
+  name = "${var.project_name}-api-task"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "api_sqs" {
+  name = "${var.project_name}-api-sqs"
+  role = aws_iam_role.api_task.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "sqs:SendMessage"
+        ]
+
+        Resource = aws_sqs_queue.test_runs.arn
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "ecs_execution" {
   role       = aws_iam_role.ecs_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
@@ -210,6 +251,7 @@ resource "aws_ecs_task_definition" "api" {
   cpu                      = 512
   memory                   = 1024
   execution_role_arn       = aws_iam_role.ecs_execution.arn
+  task_role_arn            = aws_iam_role.api_task.arn
 
   container_definitions = jsonencode([
     {
@@ -237,7 +279,12 @@ resource "aws_ecs_task_definition" "api" {
         {
           name  = "APP_FRONTEND_URL"
           value = "https://${aws_cloudfront_distribution.frontend.domain_name}"
+        },
+        {
+          name  = "APP_SQS_QUEUE_URL"
+          value = aws_sqs_queue.test_runs.url
         }
+
       ]
 
       secrets = [
